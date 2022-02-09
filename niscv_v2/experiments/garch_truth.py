80099d791a904f4b8e88d636d64f82d7411d3dd6
IanFla <51277149+IanFla@users.noreply.github.com>
2022-02-09 16:13:48 +0800

    shang

--- niscv_v2/experiments/garch_truth.py
+++ niscv_v2/experiments/garch_truth.py
@@ -1,6 +1,13 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from niscv_v2.basics.garch import GARCH
+from niscv_v2.basics.qtl import Qtl
+import scipy.stats as st
+
+import multiprocessing
+import os
+from functools import partial
+from datetime import datetime as dt
+import pickle
 
 
 class IP:
@@ -9,19 +16,72 @@ class IP:
         self.rvs = rvs
 
 
-def garch_model(d):
+def garch_model(D):
     garch = GARCH()
     garch.laplace(inflate=2, df=1)
     target = lambda x: garch.target(x[:, :3], x[:, 3:])
     statistic = lambda x: x[:, 3:].sum(axis=1)
     proposal = IP(pdf=lambda x: garch.proposal(x[:, :3], x[:, 3:]),
-                  rvs=lambda size: np.hstack(garch.predict(d, size)))
+                  rvs=lambda size: np.hstack(garch.predict(D, size)))
     return target, statistic, proposal
 
 
+def random_walk(target, x0, cov, factor, burn, size, thin):
+    walk = lambda x: st.multivariate_normal(mean=x, cov=(factor ** 2) * cov).rvs()
+    for b in range(burn):
+        x1 = walk(x0)
+        if target(x1) / target(x0) >= st.uniform.rvs():
+            x0 = np.copy(x1)
+
+    xs = []
+    for s in range(size):
+        for t in range(thin):
+            x1 = walk(x0)
+            if target(x1) / target(x0) >= st.uniform.rvs():
+                x0 = np.copy(x1)
+
+        xs.append(x0)
+
+    return np.array(xs)
+
+
+def experiment(it, D, size):
+    print(it)
+    target, statistic, proposal = garch_model(D)
+    qtl = Qtl(D + 3, target, statistic, None, proposal, size_est=None, show=False)
+    samples = qtl.ini_rvs(100000)
+    weights = target(samples) / (qtl.ini_pdf(samples) + 1.0 * (qtl.ini_pdf(samples) == 0))
+    mean = np.sum(weights * samples.T, axis=1) / np.sum(weights)
+    cov = np.cov(samples.T, aweights=weights)
+    target2 = lambda x: target(x.reshape([1, -1]))[0]
+    samples2 = random_walk(target=target2, x0=mean, cov=cov, factor=1.7 / np.sqrt(D + 3),
+                           burn=1000, size=size, thin=10)
+    statistics = statistic(samples2)
+    return statistics
+
+
+def run(D):
+    os.environ['OMP_NUM_THREADS'] = '1'
+    with multiprocessing.Pool(processes=60) as pool:
+        begin = dt.now()
+        its = np.arange(60)
+        R = pool.map(partial(experiment, D=D, size=100000000), its)
+        end = dt.now()
+        print((end - begin).seconds)
+
+    return R
+
+
 def main():
-    garch = GARCH()
-    garch.laplace(inflate=2, df=1)
+    results = []
+    for D in [1, 2, 5]:
+        R = np.array(run(D)).flatten()
+        result = [np.quantile(R, 0.05), np.quantile(R, 0.01)]
+        results.append(result)
+
+    print(results)
+    with open('../data/real/truth', 'wb') as file:
+        pickle.dump(R, file)
 
 
 if __name__ == '__main__':
