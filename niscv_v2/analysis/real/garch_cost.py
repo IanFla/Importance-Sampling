04e01cf5dd155cd5efac519752180f5aec84522f
IanFla <51277149+IanFla@users.noreply.github.com>
2022-02-12 16:21:56 +0800

    abc

---
+++ niscv_v2/analysis/real/garch_cost.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle
+
+
+def read():
+    file = open('../../data/real/garch_cost', 'rb')
+    data = np.array(pickle.load(file))
+    dataf = [df.seconds + df.microseconds / 1000000 for df in data.flatten()]
+    return np.array(dataf).reshape(data.shape)
+
+
+def main():
+    data = read().sum(axis=0)
+    group = [[0], [1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 2, 6, 7], [1, 2, 6, 8]]
+    data = np.array([data[:, gp].sum(axis=1) for gp in group])
+    labels = ['IIS', 'NIS', 'MIS$^*$', 'MIS', 'RIS', 'MLE']
+    scenarios = ['(1, 0.05)', '(1, 0.01)', '(2, 0.05)', '(2, 0.01)', '(5, 0.05)', '(5, 0.01)']
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots(figsize=[5, 3])
+    for i, dat in enumerate(data):
+        ax.plot(scenarios, dat, label=labels[i])
+
+    ax.legend(loc=2)
+    fig.tight_layout()
+    fig.show()
+    with open('../../data/real/time', 'wb') as file:
+        pickle.dump(data, file)
+
+
+if __name__ == '__main__':
+    main()
--- niscv_v2/analysis/real/garch_estimate.py
+++ niscv_v2/analysis/real/garch_estimate.py
@@ -16,6 +16,10 @@ def main():
     plt.style.use('ggplot')
     file = open('../../data/real/truth', 'rb')
     truth = np.array(pickle.load(file)).reshape([1, 6, 1])
+
+    file = open('../../data/real/time', 'rb')
+    time = pickle.load(file)
+
     data = read(24)
     mean = np.mean(data, axis=0)
     print(mean)
@@ -23,15 +27,20 @@ def main():
     colors = ['b', 'y', 'g', 'r', 'm']
     scenarios = ['(1, 0.05)', '(1, 0.01)', '(2, 0.05)', '(2, 0.01)', '(5, 0.05)', '(5, 0.01)']
     nMSE = 400000 * np.mean((data - truth) ** 2, axis=0)
+    nMSE_time = nMSE * time.T
     nVar = 400000 * np.var(data, axis=0)
     nMSE = nMSE[:, 1:] / nMSE[:, 0].reshape([-1, 1])
+    nMSE_time = nMSE_time[:, 1:] / nMSE_time[:, 0].reshape([-1, 1])
     nVar = nVar[:, 1:] / nVar[:, 0].reshape([-1, 1])
-    fig, ax = plt.subplots(figsize=[5, 3])
+    fig, ax = plt.subplots(1, 2, figsize=[10, 3])
     for i, est in enumerate(estimators):
-        ax.semilogy(scenarios, nMSE[:, i], c=colors[i], label=est)
-        ax.semilogy(scenarios, nVar[:, i], '.', c=colors[i])
+        ax[0].semilogy(scenarios, nMSE[:, i], c=colors[i], label=est)
+        ax[0].semilogy(scenarios, nVar[:, i], '.', c=colors[i])
+        ax[1].semilogy(scenarios, nMSE_time[:, i], c=colors[i], label=est)
 
-    ax.legend(loc=2)
+    for a in ax:
+        a.legend(loc=2)
+
     fig.tight_layout()
     fig.show()
     print(nMSE[:, 1:] / nMSE[:, :-1])
