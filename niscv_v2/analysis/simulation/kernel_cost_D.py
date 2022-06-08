3befb9d32cf07028e4cbec1aaddf3b5a7b14e841
IanFla <51277149+IanFla@users.noreply.github.com>
2022-06-08 17:28:08 +0800

    abc

--- niscv_v2/analysis/simulation/kernel_cost_D.py
+++ niscv_v2/analysis/simulation/kernel_cost_D.py
@@ -12,10 +12,11 @@ def read(dim):
 
 def draw(dim, ax):
     data = read(dim).sum(axis=0)
-    group = [[1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 6]]
+    group = [[1, 3], [1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 6]]
     data = np.array([data[:, gp].sum(axis=1) for gp in group])
+    data = data[1:] / data[0]
     size_kns = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
-    labels = ['NIS/DIS', 'REG', 'MLE']
+    labels = ['DIS', 'REG', 'MLE']
     colors = ['g', 'r', 'b']
     for i, dat in enumerate(data):
         ax.plot(size_kns, dat, label=labels[i], color=colors[i])
@@ -27,12 +28,13 @@ def main():
     ax = ax.flatten()
     for i, dim in enumerate([4, 6, 8]):
         draw(dim, ax[i])
-        ax[i].plot([50, 500], [0, 0], color='k')
+        # ax[i].plot([50, 500], [0, 0], color='k')
         ax[i].set_title('$d={}$'.format(dim))
         ax[i].legend(loc=2)
         if dim == 4:
-            ax[i].set_ylabel('Time')
+            ax[i].set_ylabel('$\mathrm{Time}_\mathrm{NIS}$')
 
+        ax[i].set_ylim([0.5, 4.5])
         ax[i].set_xlabel('$m$')
 
     fig.tight_layout()
--- niscv_v2/analysis/simulation/sample_cost_D.py
+++ niscv_v2/analysis/simulation/sample_cost_D.py
@@ -12,10 +12,11 @@ def read(dim):
 
 def draw(dim, ax):
     data = read(dim).sum(axis=0)
-    group = [[1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 6]]
+    group = [[1, 3], [1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 6]]
     data = np.array([data[:, gp].sum(axis=1) for gp in group])
+    data = data[1:] / data[0]
     size_ests = [1000, 2000, 3000, 5000, 7000, 10000, 20000, 30000, 50000, 70000, 100000]
-    labels = ['NIS/DIS', 'REG', 'MLE']
+    labels = ['DIS', 'REG', 'MLE']
     colors = ['g', 'r', 'b']
     for i, dat in enumerate(data):
         ax.plot(size_ests, dat, label=labels[i], color=colors[i])
@@ -27,12 +28,13 @@ def main():
     ax = ax.flatten()
     for i, dim in enumerate([4, 6, 8]):
         draw(dim, ax[i])
-        ax[i].plot([1000, 100000], [0, 0], color='k')
+        # ax[i].plot([1000, 100000], [0, 0], color='k')
         ax[i].set_title('$d={}$'.format(dim))
         ax[i].legend(loc=2)
         if dim == 4:
-            ax[i].set_ylabel('Time')
+            ax[i].set_ylabel('$\mathrm{Time}_\mathrm{NIS}$')
 
+        ax[i].set_ylim([0.5, 4.5])
         ax[i].set_xlabel('$n$')
 
     fig.tight_layout()
